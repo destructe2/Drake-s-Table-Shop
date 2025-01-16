202a81078b127d257042d4cb9d746fc8237cde0a
destructe2 <fadrah@wgu.edu>
2025-01-15 23:38:41 -0800

    Part C Rework Part E Commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-Part C. Line 35-170 on mainscreen.html written for Part C
+Part C. Line 35-55 on mainscreen.html written for Part C
 
 Part D.I added the about mapping under the "addInhousePartController" from line 32- line 34 and created an about.html 
 document with the about page content. 
+
+Part E. Line 46-103 on bootStrapData.java writting for Part E to add products and parts, Line 31-56 and Line 91-101 in
+the Part.java file to add min and max methods. 
--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,8 +1,10 @@
 package com.example.demo.bootstrap;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
 import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
+import com.example.demo.repositories.InhousePartRepository;
 import com.example.demo.repositories.OutsourcedPartRepository;
 import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
@@ -29,16 +31,76 @@ public class BootStrapData implements CommandLineRunner {
     private final ProductRepository productRepository;
 
     private final OutsourcedPartRepository outsourcedPartRepository;
+    private final InhousePartRepository inhousePartRepository;
 
-    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository) {
+    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository, InhousePartRepository inhousePartRepository) {
         this.partRepository = partRepository;
         this.productRepository = productRepository;
         this.outsourcedPartRepository=outsourcedPartRepository;
+        this.inhousePartRepository = inhousePartRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        if (partRepository.count() == 0) {
+            InhousePart tableLeg = new InhousePart();
+
+            tableLeg.setName("tableLeg");
+            tableLeg.setPrice(15.99);
+            tableLeg.setInv(30);
+            tableLeg.setMaxInv(50);
+            tableLeg.setMinInv(4);
+            partRepository.save(tableLeg);
+
+            InhousePart tableTopWood = new InhousePart();
+            tableTopWood.setName("Table Top(wood)");
+            tableTopWood.setPrice(25.99);
+            tableTopWood.setInv(15);
+            tableTopWood.setMaxInv(30);
+            tableTopWood.setMinInv(1);
+            partRepository.save(tableTopWood);
+
+
+            InhousePart tableTopGlass = new InhousePart();
+            tableTopGlass.setName("Table Top(Glass)");
+            tableTopGlass.setPrice(49.99);
+            tableTopGlass.setInv(15);
+            tableTopGlass.setMaxInv(30);
+            tableTopGlass.setMinInv(1);
+            partRepository.save(tableTopGlass);
+
+            InhousePart screwSet = new InhousePart();
+            screwSet.setName("Screw Set");
+            screwSet.setPrice(4.99);
+            screwSet.setInv(45);
+            screwSet.setMaxInv(100);
+            screwSet.setMinInv(1);
+            partRepository.save(screwSet);
+
+            InhousePart steelBracket = new InhousePart();
+            steelBracket.setName("Steel Bracket");
+            steelBracket.setPrice(25.99);
+            steelBracket.setInv(15);
+            steelBracket.setMaxInv(30);
+            steelBracket.setMinInv(1);
+            partRepository.save(steelBracket);
+
+
+        }
+        if (productRepository.count() == 0) {
+
+            Product diningTable = new Product("Dining Table", 299.99, 10);
+            productRepository.save(diningTable);
+            Product coffeeTable = new Product("Coffee Table", 199.99, 15);
+            productRepository.save(coffeeTable);
+            Product officeTable = new Product("Office Table", 399.99, 10);
+            productRepository.save(officeTable);
+            Product outdoorTable = new Product("Outdoor Table", 249.99, 10);
+            productRepository.save(outdoorTable);
+            Product glassTable = new Product("Glass Table", 399.99, 10);
+            productRepository.save(glassTable);
+        }
        /*
         OutsourcedPart o= new OutsourcedPart();
         o.setCompanyName("Western Governors University");
@@ -74,4 +136,5 @@ public class BootStrapData implements CommandLineRunner {
         System.out.println(partRepository.findAll());
 
     }
+
 }
